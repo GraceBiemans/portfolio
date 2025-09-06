1e272e29c85adf4e497e6a7e6f56223a19e7e57e
GraceBiemans <161159820+GraceBiemans@users.noreply.github.com>
2025-09-06 12:26:06 -0600

    Increase top padding on mobile pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HomeNavButtons from "@/components/HomeNavButtons";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-rose-50 via-pink-50 to-purple-50">
-      <main className="container mx-auto px-4 py-16 sm:px-6 lg:px-8 pt-24">
+      <main className="container mx-auto px-4 py-16 sm:px-6 lg:px-8 pt-32 sm:pt-24">
 
         <Profile />
 
